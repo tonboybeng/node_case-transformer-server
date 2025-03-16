56508540a6a20fd2244a08c60a1289758f3a2a41
Toni_Adrenalin <toni.toni@adrenalinmedia.com.au>
2025-03-16 17:51:02 +1100

    Solution

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
+const http = require('http');
+
 const { createServer } = require('./createServer');
 
 createServer().listen(5700, () => {
   // eslint-disable-next-line no-console
   console.log('Server started! 🚀');
 });
+
+const req = http.request('http://localhost:5700/?toCase=CAMEL', (res) => {
+  res.setEncoding('utf8');
+
+  res.on('data', (data) => {
+    console.log(data);
+  });
+});
+
+req.on('error', (error) => {
+  console.log(error);
+});
+
+req.end();
